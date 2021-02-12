c0a7353cfd96d40bc3f8a40541ab54513db00baf
Diksha  Rajput <67539159+Diksha8448@users.noreply.github.com>
2021-02-12 22:27:04 +0530

    includes added

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+<footer>
+    <div class="container">
+        <center>
+            <table style="width:100%">
+                <tr>
+                    <th>INFORMATION</th>
+                    <th>MY ACCOUNT</th>
+                    <th>CONTACT US</th>
+                </tr>
+                <tr>
+                    <td href="about.php">About us</td>
+                    <td>Login</td>
+                    <td>Contact :+91 123-000000</td>
+                </tr>
+                <tr>
+                    <td>Contact Us</td>
+                    <td>Sign UP</td>
+                    <td></td>
+                </tr>
+            </table>
+
+            	
+        </center>
+    </div>
+</footer>
---
+++ includes/header.php
@@ -0,0 +1,85 @@
+<html>
+<head>
+    <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Bootstrap Core CSS 
+  <link href="css/bootstrap.css" rel="stylesheet">  -->
+        <!-- Custom CSS -->
+        
+        <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
+
+  </head>
+<body>
+
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>                        
+            </button>
+            <a class="navbar-brand" href="index.php">Lifestyle Store</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <?php
+                if (isset($_SESSION['email'])) {
+                    ?>
+                    <li><a href = "cart.php"><span class = "glyphicon glyphicon-shopping-cart"></span> Cart </a></li>
+                    <li><a href = "settings.php"><span class = "glyphicon glyphicon-user"></span> Settings</a></li>
+                    <li><a href = "logout_script.php"><span class = "glyphicon glyphicon-log-in"></span> Logout</a></li>
+                    
+                    <?php
+                } else {
+                    ?>
+                    <li><a href="signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li><a href="#" data-target="#myModal" data-toggle="modal" ><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a href="about.php"><span class="glyphicon glyphicon-tasks"></span> About Us</a></li>
+                    <li><a href="contact.php"><span class="glyphicon glyphicon-phone"></span> Contact Us</a></li>
+                        <?php
+                    }
+                    ?>
+                    
+                    
+            </ul>
+         
+       </div>
+        
+        
+    </div>
+ <!--- start  -->
+          <!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+        <!--  end   -->
+                   
+</div>
+<sricpt>
+  
+</sricpt>
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
